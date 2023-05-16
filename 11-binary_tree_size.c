d7b33a04ac2b500a19a43fbce7175f592862a830
Holberton <holberton@Holbertons-iMac.local>
2023-05-16 11:21:59 -0500

    add tree sizebetty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,12 +1,14 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - function that return the size 
+ * binary_tree_size - function that return the size
  * @tree: tree to check
  * Return: size of tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0, r = 0, l = 0;
+	size_t size = 0; 
+	size_t sizeRight = 0; 
+	size_t sizeLeft = 0;
 
 	if (tree == NULL)
 	{
@@ -14,9 +16,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	}
 	else
 	{
-		l = binary_tree_size(tree->left);
-		r = binary_tree_size(tree->right);
-		size = r + l + 1;
+		sizeLeft = binary_tree_size(tree->left);
+		sizeRight = binary_tree_size(tree->right);
+		size = sizeRight + sizeLeft + 1;
 	}
 	return (size);
 }
