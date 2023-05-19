2ddeda39b9399b375cbec1db6f8a63f1d854954d
CarlosAlfredo4200 <95715411+CarlosAlfredo4200@users.noreply.github.com>
2023-05-19 10:34:15 -0500

    Update 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,8 +6,8 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0; 
-	size_t sizeRight = 0; 
+	size_t size = 0;
+	size_t sizeRight = 0;
 	size_t sizeLeft = 0;
 
 	if (tree == NULL)
